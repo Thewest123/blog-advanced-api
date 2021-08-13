e4ecae7b120c941aef4061d609354eb6a697755d
Jan Černý <info@jancerny.dev>
2021-08-13 14:46:37 +0200

    Added example data

---
+++ app/core/management/commands/fill_example_data.py
@@ -0,0 +1,18 @@
+import time
+
+from django.db import connection
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    """Django command that waits for database to be available"""
+
+    def handle(self, *args, **options):
+        """Handle the command"""
+        call_command('loaddata', 'example_data_fixture.json')
+
+        self.stdout.write(self.style.SUCCESS('Example data inserted!'))
+        self.stdout.write(self.style.SUCCESS(
+            'Login with admin@example.com::admin'))
