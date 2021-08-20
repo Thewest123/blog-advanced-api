4b940e45fb0d48ddffdf5359ad9d981d40a6476a
Jan Černý <info@jancerny.dev>
2021-08-20 15:43:58 +0200

    Added command to fill empty geolocations

--- app/core/management/commands/fill_empty_locations.py
+++ app/core/management/commands/fill_empty_locations.py
@@ -1,20 +1,36 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
+
+from core.utils import get_geolocation_from_address
 
 
 class Command(BaseCommand):
     """
-    Django command that for each user bulk fills
-    the location point(latitude and langitude) from adress,
-    if empty
+    Django command that bulk fills for all users
+    new location point (lat, lng) if they have the default point (0, 0)
+    and new address is set
     """
 
     def handle(self, *args, **options):
         """Handle the command"""
-        users = get_user_model().objects.filter(location_point__isnull=True)
+
+        # Get users with default location point (0, 0)
+        users = get_user_model().objects.filter(
+            location_point__distance_lte=(Point(0, 0), Distance(mm=0)))
         for user in users:
-            # TODO: Reverse geocoding API call
-            user.location_point = Point(12.3456, 12.3456)
-            user.save()
-            print(f'Obtained lat+lng for user {user}')
+
+            # If user has address to geolocate
+            if user.address:
+                api_call = get_geolocation_from_address(user.address)
+
+                user.location_point = api_call['point']
+                user.address = api_call['address']
+                user.save()
+
+                print(f'USER: {user} | New location: {user.address} ',
+                      f'[{user.location_point.y}, {user.location_point.x}] ')
+
+            else:
+                print(f'USER: {user} | NO new location!')
